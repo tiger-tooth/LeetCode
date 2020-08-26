895a5aed0480093620ede79d3a9504fe28ada957
HongyangLiu <16132238+tiger-tooth@users.noreply.github.com>
2020-08-26 22:58:27 +0800

    Create 20.有效的括号.py

---
+++ 20.有效的括号.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic: stack.append(c)
+            elif dic[stack.pop()] != c: return False 
+        return len(stack) == 1
+nums = ''
+t = Solution()
+print(t.isValid(nums))
